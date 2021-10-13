bc36e89efa4a9cab36bb178628b13c48109b701f
scorpio2774 <teoharle31@gmail.com>
2021-10-13 09:12:11 +0200

    a

--- sortiranje/sortiranje/Program.cs
+++ sortiranje/sortiranje/Program.cs
@@ -11,36 +11,49 @@ namespace sortiranje
         static void Main(string[] args)
         {
             int n = 50000;
-            int[] tab = new int[n];
+            double[] tab = new double[n];
+            //int[] tab = {6,24,4,80,19,1,84,10,13,7};
             Random r = new Random();
 
             for(int k = 0; k < n; k++){
-                tab[k] = r.Next();
+                tab[k] = r.NextDouble();
             }
 
-            DateTime d = DateTime.Now;
-            Vstavljanje(tab);
-            DateTime d1 = DateTime.Now;
-            TimeSpan ts = d1 - d;
-            Console.WriteLine("Čas za vstavljanje " + ts.TotalMilliseconds + "ms");
+            //DateTime d = DateTime.Now;
+            //Vstavljanje(tab);
+            //DateTime d1 = DateTime.Now;
+            //TimeSpan ts = d1 - d;
+            //Console.WriteLine("Čas za vstavljanje " + ts.TotalMilliseconds + "ms");
 
-            for (int k = 0; k < n; k++)
-            {
-                tab[k] = r.Next();
-            }
+            //for (int k = 0; k < n; k++)
+            //{
+            //    tab[k] = r.Next();
+            //}
 
-             d = DateTime.Now;
-            Izbiranje(tab);
-             d1 = DateTime.Now;
-             ts = d1 - d;
-            Console.WriteLine("Čas za izbiranje " + ts.TotalMilliseconds + "ms");
+            // d = DateTime.Now;
+            //Izbiranje(tab);
+            // d1 = DateTime.Now;
+            // ts = d1 - d;
+            //Console.WriteLine("Čas za izbiranje " + ts.TotalMilliseconds + "ms");
 
             //Console.WriteLine("Neurejena");
             //Izpis(tab);
             ////Izbiranje(tab);
             //Vstavljanje(tab);
             //Console.WriteLine("Urejeno");
+
+            //Console.WriteLine("Neurejena");
             //Izpis(tab);
+            //Console.WriteLine("");
+            
+            //d = DateTime.Now;
+            //QuickSort(0, tab.Length - 1, tab);
+            //d1 = DateTime.Now;
+            //ts = d1 - d;
+            //Console.WriteLine("Čas za quicksort " + ts.TotalMilliseconds + "ms");
+            //Console.WriteLine("");
+            //Console.WriteLine("Urejeno");
+            IzpisDouble(tab);
             Console.ReadLine();
         }
 
@@ -74,8 +87,66 @@ namespace sortiranje
             }
         }
 
+        static int Pivot(int zac, int konec, double[] tab) {
+            double p = tab[zac];
+            int m = zac;
+            int n = konec + 1;
+
+            //poišči z m prvega, ki je večji od p
+            do
+            {
+                m = m + 1;
+            } while (tab[m] <= p & m < konec);
+            do
+            {
+                n = n - 1;
+            } while (tab[n] > p);
+            //tab[m] je večji od p
+            //tab od n je manjši od p
+            //zamenjaj jih
+            while (m < n)
+            {
+                double temp = tab[m];
+                tab[m] = tab[n];
+                tab[n] = temp;
+                do { m = m + 1; } while (tab[m] <= p);
+                do { n = n - 1; } while (tab[n] > p);
+
+            }
+
+            // zamenjaj pivotni element z elementom tab[n]
+            double temp2 = tab[n];
+            tab[n] = tab[zac];
+            tab[zac] = temp2;
+            //Izpis(tab);
+            return n;
+
+        }
+
+        static void QuickSort(int zac, int konc, double[] tab) {
+            if (zac >= konc) {
+                
+                return; 
+            }
+            
+            int a = Pivot(zac, konc, tab);
+            
+            QuickSort(zac, a - 1, tab);//levi del
+            QuickSort(a + 1, konc, tab);
+        }
+
+
         static void Izpis(int[] a) {
             for (int k = 0; k < a.Length; k++) {
+                Console.Write(a[k] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        static void IzpisDouble(double[] a)
+        {
+            for (int k = 0; k < a.Length; k++)
+            {
                 Console.Write(a[k] + "\t");
             }
             Console.WriteLine();
