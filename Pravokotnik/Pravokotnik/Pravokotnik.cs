6eab93dfb9906dd49cd59df4bd65cb11dd81ebb9
scorpio2774 <teoharle31@gmail.com>
2021-10-22 12:29:11 +0200

    Pravokotnik overriding, overloading

---
+++ Pravokotnik/Pravokotnik/Pravokotnik.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pravokotnik
+{
+    class Pravokotnik
+    {
+        private double Širina;
+        private double Višina;
+
+        public Pravokotnik() {
+            Širina = 0;
+            Višina = 0;
+        }
+
+        public Pravokotnik(double a, double b) {
+            Širina = a;
+            Višina = b;
+        }
+
+
+
+        public double Višina1 { get => Višina; set => Višina = value; }
+        public double Širina1 { get => Širina; set => Širina = value; }
+
+        public double Ploščina { get => Širina * Višina; }
+
+        public static bool operator ==(Pravokotnik a, Pravokotnik b) {
+            return a.Ploščina == b.Ploščina;
+        }
+        public static bool operator !=(Pravokotnik a, Pravokotnik b) {
+           return a.Ploščina != b.Ploščina;
+        }
+
+        public static bool operator >(Pravokotnik a, Pravokotnik b) {
+            return a.Ploščina > b.Ploščina;
+        }
+
+        public static bool operator <(Pravokotnik a, Pravokotnik b)
+        {
+            return a.Ploščina < b.Ploščina;
+        }
+        public override string ToString()
+        {
+            return "Višina je " + Višina + ", Širina je " + Širina;
+        }
+
+    }
+}
---
+++ Pravokotnik/Pravokotnik/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pravokotnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pravokotnik p1 = new Pravokotnik();
+            Pravokotnik p2 = new Pravokotnik();
+            Pravokotnik p3 = new Pravokotnik(10, 15);
+            p1.Širina1 = 10;
+            p1.Višina1 = 15;
+            p2.Širina1 = 25;
+            p2.Višina1 = 10;
+            Console.WriteLine("Pravokotnik 1 " + p1.ToString());
+            Console.WriteLine("Pravokotnik 2 " + p2.ToString());
+            Console.WriteLine("Pravokotnik 3 " + p3.ToString());
+            if (p1 == p2)
+                Console.WriteLine("p1=p2");
+            else
+            {
+                if (p1 > p2)
+                    Console.WriteLine("P1 je večji od P2");
+                else
+                    Console.WriteLine("P2 je večji od P1");
+            }
+            if (p1 == p3)
+                Console.WriteLine("P1 je enak P3");
+            Console.ReadLine();
+        }
+    }
+}
