0ce44fad17487d6a9c817748f031794a2a68201f
scorpio2774 <teoharle31@gmail.com>
2021-11-03 08:47:25 +0100

    racunanje matrik

---
+++ Racunanje1/Racunanje1/Matrika.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racunanje1
+{
+    class Matrika
+    {
+        int[,] m = new int[3, 3];
+        public int this[int k, int j] {
+            get { return m[k, j]; }
+            set { m[k, j] = value; }
+        }
+
+        //izpis
+        public override string ToString()
+        {
+            string s = "";
+            for (int k = 0; k < 3; k++) {
+                for (int j = 0; j < 3; j++) {
+                    s += m[k, j] + ",\t";
+                }
+                s += "\n"; // s += Environment.NewLine;
+            }
+            return s;
+        }
+
+        public static Matrika operator +(Matrika a, Matrika b) {
+            Matrika r = new Matrika();
+            for (int k = 0; k < 3; k++) {
+                for (int j = 0; j < 3; j++) {
+                    r[k, j] = a[k, j] + b[k, j];
+                }
+            }
+            return r;
+        }
+
+        public static Matrika operator *(Matrika a, Matrika b) {
+            Matrika r = new Matrika();
+            for (int k = 0; k < 3; k++) {
+                for (int j = 0; j < 3; j++) {
+                    r[k, j] = a[k, 0] * b[0, j] + a[k, 1] * b[1, j] + a[k, 2] * b[2, j];
+                }
+            }
+            return r;
+        }
+
+    }
+}
---
+++ Racunanje1/Racunanje1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racunanje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrika x = new Matrika();
+            x[0, 0] = 1; x[0, 1] = -1; x[0, 2] = 1;
+            x[1, 0] = 2; x[1, 1] = 0; x[1, 2] = -1;
+            x[2, 0] = 3; x[2, 1] = 2; x[2, 2] = 1;
+            Console.WriteLine(x.ToString());
+
+            Console.WriteLine("*");
+
+            Matrika y = new Matrika();
+            y[0, 0] = 1; y[0, 1] = 2; y[0, 2] = 3;
+            y[1, 0] = 0; y[1, 1] = 0; y[1, 2] = 0;
+            y[2, 0] = 1; y[2, 1] = 1; y[2, 2] = 1;
+            Console.WriteLine(y.ToString());
+            Console.WriteLine("=");
+            Console.WriteLine((x*y).ToString());
+            Console.WriteLine("Seštevanje");
+
+            Console.WriteLine(x.ToString());
+            Console.WriteLine("+");
+            Console.WriteLine(y.ToString());
+            Console.WriteLine("=");
+            Console.WriteLine((x + y).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
