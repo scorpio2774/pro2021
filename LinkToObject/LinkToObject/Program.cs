4b73a80bd8278097365899bf291a3a5834c5baf8
scorpio2774 <teoharle31@gmail.com>
2021-12-01 08:51:15 +0100

    Linq to object

--- LinkToObject/LinkToObject/Program.cs
+++ LinkToObject/LinkToObject/Program.cs
@@ -50,6 +50,73 @@ namespace LinkToObject
             //    Console.WriteLine(d);
 
             List<Naročilo> naročilaList = setupNaročila();
+            var x1 = from n in naročilaList
+                     select n;
+            foreach (var y in x1) {
+                Console.WriteLine(y.NaročiloID + " " + y.Datum);
+
+            }
+            Console.WriteLine("__________________________________________________________________");
+            var x2 = from n in naročilaList
+                     select n.Datum;
+            foreach (var y in x2)
+            {
+                Console.WriteLine(y.ToShortDateString());
+
+            }
+            Console.WriteLine("__________________________________________________________________");
+            var x3 = from n in naročilaList
+                     select new { Dan = n.Datum, ID = n.NaročiloID };
+            foreach (var y in x3)
+            {
+                Console.WriteLine(y.ID + " " + y.Dan);
+
+            }
+            Console.WriteLine("__________________________________________________________________");
+
+            var x4 = from n in naročilaList
+                     from n1 in n.Elementi
+                     select n1;
+            foreach (var y in x4)
+            {
+                Console.WriteLine(y.ElementID+" "+y.ImeIzdelka+" " + y.Količina);
+
+            }
+            Console.WriteLine("__________________________________________________________________");
+
+            var x5 = from n in naročilaList
+                     where n.NaročiloID==9010
+                     select n;
+            foreach (var y in x5)
+            {
+                Console.WriteLine(y.NaročiloID+" "+y.Datum);
+
+            }
+            Console.WriteLine("__________________________________________________________________");
+
+            var x6 = from n in naročilaList
+                     where n.Elementi.Count()>2
+                     select n;
+            foreach (var y in x6)
+            {
+                Console.WriteLine(y.NaročiloID + " " + y.Datum);
+
+            }
+            Console.WriteLine("__________________________________________________________________");
+
+            int[] števila = { 46, 24, 79, 35, 12, 57, 80, 68, 46 };
+            var izraz = števila.Count();
+            Console.WriteLine("Števil je " + izraz);
+            var izraz1 = števila.Sum();
+            Console.WriteLine("Vsota je " + izraz1);
+            var izraz2 = števila.Min();
+            Console.WriteLine("Minimmum je " + izraz2);
+            var izraz3 = števila.FirstOrDefault();
+            Console.WriteLine("Prvo je " + izraz3);
+            var izraz4 = števila.Any();
+            Console.WriteLine("Ali je polno? " + izraz4);
+            var izraz5 = števila.Distinct().Count();
+            Console.WriteLine("Različnih je " + izraz5);
 
             Console.ReadLine();
         }
