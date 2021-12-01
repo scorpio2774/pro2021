1c06ffa07a20fc9694282d6d370fd432bc3e287d
scorpio2774 <teoharle31@gmail.com>
2021-12-01 10:05:10 +0100

    Linq vaja 2

---
+++ PoizvedbeVDrevesu/PoizvedbeVDrevesu/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTree;
+namespace PoizvedbeVDrevesu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Delaj();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+
+        }
+
+        private static void Delaj()
+        {
+            Tree<Zaposleni> z = new Tree<Zaposleni>(
+               new Zaposleni { Id = 1, Ime = "Miha", Priimek = "Polanc", Oddelek = "IT" });
+            z.Insert(new Zaposleni { Id = 2, Ime = "Andrej", Priimek = "Bratina", Oddelek = "Marketing" });
+            z.Insert(new Zaposleni { Id = 4, Ime = "Lucija", Priimek = "Krkoč", Oddelek = "Prodaja" });
+            z.Insert(new Zaposleni { Id = 6, Ime = "Peter", Priimek = "Gulin", Oddelek = "IT" });
+            z.Insert(new Zaposleni { Id = 3, Ime = "Franc", Priimek = "Milčinski", Oddelek = "Marketing" });
+            z.Insert(new Zaposleni { Id = 5, Ime = "Pavel", Priimek = "Matko", Oddelek = "Prodaja" });
+
+
+            
+            
+            //1. napiši kodo, ki izpiše vse oddelke
+            //2. napiši kodo, ki izpiše vse različne oddelke
+            var x = from a in z
+                    select a.Oddelek;
+            x = x.Distinct();
+            Console.WriteLine("Oddelki:");
+
+            foreach (var y in x)
+                Console.WriteLine(y);
+            Console.WriteLine("________________________________________");
+            //3. napiši kodo, ki izpiše vse zaposlene v oddelku IT
+            var x1 = from a in z
+                     where a.Oddelek == "IT"
+                     select a;
+            Console.WriteLine("zaposleni v IT");
+            foreach (var y in x1)
+                Console.WriteLine(y.ToString());
+            Console.WriteLine("________________________________________");
+            //4. napiši kodo, ki izpiše zaposlene po oddelkih
+            var x2 = from a in z
+                     group a by a.Oddelek;
+            Console.WriteLine("zaposleni po oddelku");
+            foreach (var y in x2)
+            {
+                Console.WriteLine(y.Key);
+                foreach (var y1 in y)
+                    Console.WriteLine("\t"+y1.ToString());
+            }
+
+            Console.WriteLine("________________________________________");
+
+            z.Insert(new Zaposleni { Id = 7, Ime = "Monika", Priimek = "Seleš", Oddelek = "Kozmetika" });
+
+            Console.WriteLine("zaposleni po oddelku po vstavljanju");
+            foreach (var y in x2)
+            {
+                Console.WriteLine(y.Key);
+                foreach (var y1 in y)
+                    Console.WriteLine("\t" + y1.ToString());
+            }
+        }
+    }
+}
---
+++ PoizvedbeVDrevesu/PoizvedbeVDrevesu/Zaposleni.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoizvedbeVDrevesu
+{
+    class Zaposleni:IComparable<Zaposleni>
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Priimek { get; set; }
+        public string Oddelek { get; set; }
+
+        public int CompareTo(Zaposleni other)
+        {
+            //zaposleni 1 j pred zaposlenim 2, če je njegov id manjši
+            if (this.Id < other.Id)
+                return -1;
+            if (this.Id > other.Id)
+                return 1;
+            return 0;
+        }
+        public override string ToString()
+        {
+            return "Id: " + Id + " Ime: " + Ime + " " + Priimek + " Oddelek: " + Oddelek;
+        }
+    }
+}
